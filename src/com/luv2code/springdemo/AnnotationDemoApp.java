90bd6de3bdd6be171034e8f86ad7ac680392137c
Hanna Lembak <lembakhanna@gmail.com>
2022-12-02 17:05:51 +0200

    Configure dependency injection with @Autoweired annotation

--- src/com/luv2code/springdemo/AnnotationDemoApp.java
+++ src/com/luv2code/springdemo/AnnotationDemoApp.java
@@ -10,11 +10,12 @@ public class AnnotationDemoApp {
                 new ClassPathXmlApplicationContext("applicationContext.xml");
 
         // get the bean from spring container
-        Coach theCoach = context.getBean("pingPongCoach", Coach.class);
+        Coach theCoach = context.getBean("tennisCoach", Coach.class);
 
         // call a method on the bean
         System.out.println(theCoach.getDailyWorkout());
 
+        System.out.println(theCoach.getDailyFortune());
         // close th context
         context.close();
     }
--- src/com/luv2code/springdemo/PingPongCoach.java
+++ src/com/luv2code/springdemo/PingPongCoach.java
@@ -1,7 +1,15 @@
 package com.luv2code.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class PingPongCoach implements Coach {
 
+    private FortuneService fortuneService;
+
+    @Autowired
+    public PingPongCoach(FortuneService fortuneService) {
+        this.fortuneService = fortuneService;
+    }
     // define a default constructor
     public PingPongCoach() {
         System.out.println(">> PingPongCoach: inside default constructor");
@@ -10,5 +18,10 @@ public class PingPongCoach implements Coach {
     @Override
     public String getDailyWorkout() {
         return "Practice your pingpong drop shot";
+    }
+
+    @Override
+    public String getDailyFortune() {
+        return fortuneService.getFortune();
     }
 }
