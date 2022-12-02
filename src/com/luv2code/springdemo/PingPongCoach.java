b3b111c7277c3266f5024843c1c99808f4bc800f
Hanna Lembak <lembakhanna@gmail.com>
2022-12-02 16:43:54 +0200

    Define a new Coach implementation using Annotations

---
+++ src/com/luv2code/springdemo/PingPongCoach.java
@@ -0,0 +1,14 @@
+package com.luv2code.springdemo;
+
+public class PingPongCoach implements Coach {
+
+    // define a default constructor
+    public PingPongCoach() {
+        System.out.println(">> PingPongCoach: inside default constructor");
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice your pingpong drop shot";
+    }
+}
